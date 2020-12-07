1f7c86fce2979730f312d4eff8534a57a3867520
Abhishek Agnihotri <agni.abhi00@gmail.com>
2020-12-07 23:21:19 +0530

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   const [nav_up, setNav_up] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
   window.addEventListener('scroll', function(){
     if (window.scrollY > 10){
       setNav_up(false);
@@ -12,25 +31,54 @@ function Navbar() {
   })
 
   return (
-    <nav className= { nav_up ? 'navbar' : 'navbar-up'}>
-      <div className='nav-list'>
-        <div className="nav-item">
-          Home
+    <>
+      <nav className={ !click & nav_up ? 'navbar' : 'navbar-up'}>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Alkesh weds Deeksha
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i class={click ? 'fa fa-times' : 'fa fa-list'} aria-hidden="true" />
+          </div>
+          <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
+
+            <li className='nav-item'>
+              <a href='#banner' className='nav-links' 
+                onClick={closeMobileMenu}>
+                Home
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a
+                href='#shop'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Functions
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a
+                href='#services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Video Message
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a href='#contact' className='nav-links' onClick={closeMobileMenu}>
+                Gallery
+              </a>
+            </li>
+
+          </ul>
         </div>
-        <div className="nav-item">
-          OUR STORY
-        </div>
-        <div className="nav-item">
-          RSVP
-        </div>
-        <div className="nav-item">
-          FUNCTIONS
-        </div>
-        <div className="nav-item">
-          GALLERY
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
