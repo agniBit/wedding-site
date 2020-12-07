166949d57aed3e568159b71ddbf09fba80194f71
Abhishek Agnihotri <agni.abhi00@gmail.com>
2020-12-07 20:32:43 +0530

    change titale

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -5,11 +5,33 @@ function RSVP() {
       <div className='rsvp' id='rsvp'>
           <div className='rsvp-container'>
             <div className='from-1'>
-                
+                <div>
+                    RSVP
+                </div>
+                <div>
+                    Vinod Trivedi
+                </div>
+                <div>
+                    Sanjay Trivedi
+                </div>
+                <div>
+                    Shailesh Trivedi
+                </div>
+                <div>
+                    Umesh Trivedi
+                </div>
+                <div>
+                    Pravin Trivedi
+                </div>
             </div>
 
             <div className='from-2'>
-
+                <div>
+                    Shashi Bhushan Trivedi
+                </div>
+                <div>
+                    Shiv Bhushan Trivedi
+                </div>
             </div>
           </div>
       </div>
